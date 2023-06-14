3f0bf0d2f996fd052d64d398d252c07eea41ea61
MaristelaP <marispeg@gmail.com>
2023-06-14 13:57:32 -0300

    primeiro commit, criado Login e imagens

---
+++ src/components/Login/login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Login = () => {
+    return (
+
+        <div>
+            <div>
+                <img src="" alt="foto de cães e gatos" />
+            </div>
+            <div>
+            <img src="" alt="logo ilustração de patinhas de pets" />
+            <p>Seja bem vindo</p>
+            </div>
+        </div>
+    )
+} 
+export default Login;
