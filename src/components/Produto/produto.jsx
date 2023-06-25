0175e7540af14b1dbaaecb68f768c4da7c693002
MaristelaP <marispeg@gmail.com>
2023-06-25 14:25:04 -0300

    estilização do componente estoque e produto

--- src/components/Produto/produto.jsx
+++ src/components/Produto/produto.jsx
@@ -30,55 +30,49 @@ const Produto = () => {
 
             <h3>Cadastro de Produto</h3>
             <form>
-            <label>Estoque: </label>
-            <select 
-            value={produto.estoque}
-            onChange={(evento) => setProduto({...produto, estoque: evento.target.value})}>
-                {estoque.map((armazem) => {
-                    return <option value={armazem.id} key={armazem.id}>{armazem.nome}</option>
-                })}
-            </select>
-
-            <label>Animal: </label>
-            <select
-            value={produto.animal} 
-            onChange={(evento) => setProduto({...produto, animal: evento.target.value})}
-            >
-                <option>...</option>
-                <option value={"cachorro"}>Cachorro</option>
-                <option value={"gato"}>Gato</option>
-            </select>
-            
-            <label>Tipo do Produto: </label>
-            <select
-            value={produto.produto} 
-            onChange={(evento) => setProduto({...produto, produto: evento.target.value})}
-            >
-                <option>...</option>
-                <option value={"ração"}>Ração</option>
-                <option value={"antipulgas"}>Antipulgas</option>
-                <option value={"antiparasitária"}>Antiparasitária</option>
-            </select>
-
-            <label>Quantidade: </label>
-            <input
-                type="number"
-                value={produto.quantidade}
-                placeholder="Quantidade do Produto"
-                onChange={(evento) => setProduto({...produto, quantidade: evento.target.value})}
-            />
-
-
-            <label>Categoria: </label>
-            <select
-            value={produto.categoria}
-            onChange={(evento) => setProduto({...produto, categoria: evento.target.value})}
-            > 
-                <option>...</option>
-                <option value={"filhote"}>Filhote</option>
-                <option value={"adulto"}>Adulto</option>
-            </select>
-            
+                    <label>Estoque: </label>
+                    <select 
+                    value={produto.estoque}
+                    onChange={(evento) => setProduto({...produto, estoque: evento.target.value})}>
+                        {estoque.map((armazem) => {
+                            return <option value={armazem.id} key={armazem.id}>{armazem.nome}</option>
+                        })}
+                    </select>
+                    <label>Animal: </label>
+                    <select
+                    value={produto.animal} 
+                    onChange={(evento) => setProduto({...produto, animal: evento.target.value})}
+                    >
+                        <option>...</option>
+                        <option value={"cachorro"}>Cachorro</option>
+                        <option value={"gato"}>Gato</option>
+                    </select>
+                    <label>Tipo do Produto: </label>
+                    <select
+                    value={produto.produto} 
+                    onChange={(evento) => setProduto({...produto, produto: evento.target.value})}
+                    >
+                        <option>...</option>
+                        <option value={"ração"}>Ração</option>
+                        <option value={"antipulgas"}>Antipulgas</option>
+                        <option value={"antiparasitária"}>Antiparasitária</option>
+                    </select>
+                    <label>Quantidade: </label>
+                    <input
+                        type="number"
+                        value={produto.quantidade}
+                        placeholder="Quantidade do Produto"
+                        onChange={(evento) => setProduto({...produto, quantidade: evento.target.value})}
+                    />
+                    <label>Categoria: </label>
+                    <select
+                    value={produto.categoria}
+                    onChange={(evento) => setProduto({...produto, categoria: evento.target.value})}
+                    > 
+                        <option>...</option>
+                        <option value={"filhote"}>Filhote</option>
+                        <option value={"adulto"}>Adulto</option>
+                    </select>
             
             <button type="button" onClick={adicionaProduto}>Confirmar Cadastro de Produto</button>
 
