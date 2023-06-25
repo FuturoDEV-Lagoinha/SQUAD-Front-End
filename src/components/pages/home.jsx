6c74e626669a0f19cab364bb372cd0f37fca7bc8
MaristelaP <marispeg@gmail.com>
2023-06-25 17:50:53 -0300

    criado estilização da home e alterações em produto

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,4 +1,5 @@
 import { useProdutoContext } from "../contexts/ProdutoContext";
+import "./home.css"
 
 
 const Home = () => {
@@ -33,37 +34,36 @@ const Home = () => {
     return (
 
         <div>
-            <div className="lista de estoques">
+            <div className="lista-de-estoques">
                 <h2>DASHBOARD: LISTA DE PRODUTOS</h2>
-
                 <p>FILHOTES</p>
-
+                <div  className="lista-de-estoques">
                 <ul>
-                {filtraFilhotes().map(produto => <li id="filhote" className="filhote-lista" key={produto.id}>ID: {produto.id}; Tipo de Produto: {produto.produto}; 
-                        Animal: {produto.animal}; Categoria: {produto.categoria}</li>)}
+                {filtraFilhotes().map(produto => (<li id="filhote" className="filhote-lista" key={produto.id}><span>ID:</span> {produto.id}<span>Tipo de Produto:</span> {produto.produto} 
+                       <span>Animal: </span>{produto.animal} <span>Categoria:</span> {produto.categoria}</li>))}
                 </ul>
 
                 <p>ADULTOS</p>
 
                 <ul>
-                {filtraAdultos().map(produto => <li id="adulto" className="adulto-lista" key={produto.id}>ID: {produto.id}; Tipo de Produto: {produto.produto}; 
-                        Animal: {produto.animal}; Categoria: {produto.categoria}</li>)}
+                {filtraAdultos().map(produto => <li id="adulto" className="adulto-lista" key={produto.id}><span>ID:</span> {produto.id}<span>Tipo de Produto:</span> {produto.produto} 
+                    <span>Animal:</span> {produto.animal} <span>Categoria:</span> {produto.categoria}</li>)}
                 </ul>
 
                 <p>GATOS</p>
 
                 <ul>
-                {filtraGatos().map(produto => <li id="gato" className="gato-lista" key={produto.id}>ID: {produto.id}; Tipo de Produto: {produto.produto}; 
-                        Animal: {produto.animal}; Categoria: {produto.categoria}</li>)}
+                {filtraGatos().map(produto => <li id="gato" className="gato-lista" key={produto.id}><span>ID:</span> {produto.id}<span>Tipo de Produto:</span> {produto.produto} 
+                    <span>Animal:</span> {produto.animal} <span>Categoria:</span> {produto.categoria}</li>)}
                 </ul>
 
                 <p>CÃES</p>
 
                 <ul>
-                {filtraCachorros().map(produto => <li id="cachorro" className="cachorro-lista" key={produto.id}>ID: {produto.id}; Tipo de Produto: {produto.produto}; 
-                        Animal: {produto.animal}; Categoria: {produto.categoria}</li>)}
+                {filtraCachorros().map(produto => <li id="cachorro" className="cachorro-lista" key={produto.id}><span>ID:</span> {produto.id}<span>Tipo de Produto:</span> {produto.produto} 
+                    <span>Animal:</span> {produto.animal} <span>Categoria:</span> {produto.categoria}</li>)}
                 </ul>
-
+                </div>
 
             </div>
         </div>
